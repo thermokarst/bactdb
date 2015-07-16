867e914bf9059eb632db3f0264c004fd55fca408
Matthew Dillon <mrdillon@alaska.edu>
2015-07-16 12:22:24 -0800

    Adding mailgun API jazz

--- serve.sh
+++ serve.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+if [ -f .env ]; then
+    source .env
+fi
+
 SECRET=secret \
 PGDATABASE=bactdbtest \
 DOMAINS="http://localhost:4200" \
+ACCOUNT_KEYS=$ACCOUNT_KEYS \
 bash -c 'go run *.go serve'
